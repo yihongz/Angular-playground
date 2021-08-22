56e9bc17847f3aeb49aa2b3b84af89a92211f272
Paulo Almeida <estpaulovictora@live.com>
2021-08-22 14:58:52 -0300

    Two-way biding

--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { DataBidingComponent } from './data-biding/data-biding.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DataBidingComponent } from './data-biding/data-biding.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
