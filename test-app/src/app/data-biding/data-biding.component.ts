9cad92296ab1c68b4e0a9530618fee1763fbe353
Paulo Almeida <estpaulovictora@live.com>
2021-08-22 14:53:17 -0300

    Event biding

--- test-app/src/app/data-biding/data-biding.component.ts
+++ test-app/src/app/data-biding/data-biding.component.ts
@@ -12,10 +12,19 @@ export class DataBidingComponent implements OnInit {
   public checkedDisabled:boolean = true;
   public imgSrc:string = "https://img.ibxk.com.br/2020/01/30/30021141299110.jpg";
   public imgTitle:string = "Título";
+  public position: {x: number, y: number} = {x: 0, y: 0};
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public alertaInfo(valor:MouseEvent):void {
+    console.log(valor);
+  }
+
+  public mouseMoveTest(valor:MouseEvent):void {
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
