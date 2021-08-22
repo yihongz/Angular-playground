c2c2ad9437ab7f81d942ee9485946c5fc374f374
Paulo Almeida <estpaulovictora@live.com>
2021-08-22 14:41:17 -0300

    Property biding

--- test-app/src/app/data-biding/data-biding.component.ts
+++ test-app/src/app/data-biding/data-biding.component.ts
@@ -9,6 +9,9 @@ export class DataBidingComponent implements OnInit {
 
   public nome:string = "Paulo";
   public idade:number = 23;
+  public checkedDisabled:boolean = true;
+  public imgSrc:string = "https://img.ibxk.com.br/2020/01/30/30021141299110.jpg";
+  public imgTitle:string = "Título";
 
   constructor() { }
 
