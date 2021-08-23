936dd630f898dc8490247bffa7e22fe6c536e753
Paulo Almeida <estpaulovictora@live.com>
2021-08-22 21:36:21 -0300

    Modules and Sub-modules

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-diretivas-atributos>
-    <h1>Diretivas de atributos</h1>
-    <hr/>
-  </app-diretivas-atributos>
+  <app-new-component></app-new-component>
   <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
