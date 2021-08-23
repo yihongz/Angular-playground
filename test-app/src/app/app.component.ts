3e13e2de76d5f3010b3dfc00178b06aef9cbe89e
Paulo Almeida <estpaulovictora@live.com>
2021-08-22 21:59:22 -0300

    @Input()

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-new-component></app-new-component>
+  <app-input [contador]="addValue"></app-input>
+  <br/>
+  <button (click)="Add()">Adicionar</button>
   <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
+  public addValue:number = 0;
+
   constructor() { }
 
   ngOnInit(): void { }
+
+  public Add():void {
+    this.addValue += 1;
+  }
 }
--- test-app/src/app/shared/shared.module.ts
+++ test-app/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewComponentComponent } from './new-component/new-component.component';
+import { InputComponent } from './input/input.component';
 
 
 
 @NgModule({
   declarations: [
-    NewComponentComponent
+    NewComponentComponent,
+    InputComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NewComponentComponent
+    NewComponentComponent,
+    InputComponent
   ]
 })
 export class SharedModule { }
