fc3e954ca7c85791fc519c5b90cac304ab38b250
Paulo Almeida <estpaulovictora@live.com>
2021-08-22 21:17:51 -0300

    ngContent

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-diretivas-atributos></app-diretivas-atributos>
+  <app-diretivas-atributos>
+    <h1>Diretivas de atributos</h1>
+    <hr/>
+  </app-diretivas-atributos>
   <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
