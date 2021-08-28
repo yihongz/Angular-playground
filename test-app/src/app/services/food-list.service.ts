13af9515ed7011901a5c69bce0beec03c9fe72f3
Paulo Almeida <estpaulovictora@live.com>
2021-08-28 19:11:55 -0300

    http custom header

--- test-app/src/app/services/food-list.service.ts
+++ test-app/src/app/services/food-list.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { FoodList } from '../module/food-list';
@@ -10,30 +10,36 @@ export class FoodListService {
 
   public emitEvent = new EventEmitter();
 
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
   private url: string = "http://localhost:3000"
   constructor(private http: HttpClient) { }
 
   public foodList(): Observable<FoodList> {
-    return this.http.get<FoodList>(`${this.url}/list-food`).pipe(res => res,
+    return this.http.get<FoodList>(`${this.url}/list-food`, this.httpOptions).pipe(res => res,
       error => error)
   }
 
   public foodListAdd(value: string): Observable<FoodList> {
-    return this.http.post<FoodList>(`${this.url}/list-food`, { nome: value }).pipe(
+    return this.http.post<FoodList>(`${this.url}/list-food`, { nome: value }, this.httpOptions).pipe(
       res => res,
       error => error
     )
   }
 
   public foodListEdit(value : string, id: number): Observable<FoodList> {
-    return this.http.put<FoodList>(`${this.url}/list-food/${id}`, { nome: value }).pipe(
+    return this.http.put<FoodList>(`${this.url}/list-food/${id}`, { nome: value }, this.httpOptions).pipe(
       res => res,
       error => error
     )
   }
 
   public foodListDelete(id: number): Observable<FoodList> {
-    return this.http.delete<FoodList>(`${this.url}/list-food/${id}`).pipe(
+    return this.http.delete<FoodList>(`${this.url}/list-food/${id}`, this.httpOptions).pipe(
       res => res,
       error => error
     )
