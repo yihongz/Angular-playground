e293c2f909635ed708b46b3e5278458154641d7c
Paulo Almeida <estpaulovictora@live.com>
2021-08-29 22:03:57 -0300

    ngForm + Selected

--- test-app/src/app/forms/forms.component.ts
+++ test-app/src/app/forms/forms.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
+  public list: Array<{ comida: string, preco: number }> = [
+    { comida: "Arroz", preco: 10 },
+    { comida: "Feijão", preco: 20.01 },
+    { comida: "Ovo", preco: 5.00 }
+  ];
   constructor() { }
 
   ngOnInit(): void {
