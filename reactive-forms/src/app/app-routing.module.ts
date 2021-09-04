79e5036ceff7b7f67f66dbd71363373981d6a7b8
Paulo Almeida <estpaulovictora@live.com>
2021-09-03 21:53:01 -0300

    Testing reactive forms of  angular

--- reactive-forms/src/app/app-routing.module.ts
+++ reactive-forms/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ReactiveFormsComponent  } from './reactive-forms/reactive-forms.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: ReactiveFormsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
