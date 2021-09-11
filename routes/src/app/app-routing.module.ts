f19ced3eeef80905d99e290e73073b0f373b8fb7
Paulo Almeida <estpaulovictora@live.com>
2021-09-11 14:14:18 -0300

    Creating routes

--- routes/src/app/app-routing.module.ts
+++ routes/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 const routes: Routes = [
   {
-    path: "",
-    component: PageHomeComponent,
-    pathMatch: ""
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent,
+  },
+  {
+    path: '404',
+    component: PageErrorComponent,
   }
 ];
 
