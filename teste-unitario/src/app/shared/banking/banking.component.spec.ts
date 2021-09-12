eec9887b00af523e3706428c5f3de1ad589b2df5
Paulo Almeida <estpaulovictora@live.com>
2021-09-12 14:29:12 -0300

    Testing if's

--- teste-unitario/src/app/shared/banking/banking.component.spec.ts
+++ teste-unitario/src/app/shared/banking/banking.component.spec.ts
@@ -8,9 +8,9 @@ describe('BankingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BankingComponent ]
+      declarations: [BankingComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -32,17 +32,33 @@ describe('BankingComponent', () => {
   });
 
   it('(U) setSacar(): should transfer poupanca from carteira', () => {
-   component.setSacar('10');
-   fixture.detectChanges(); // De unidade não precisa
+    component.setSacar('10');
+    fixture.detectChanges(); // De unidade não precisa
 
-   expect(component.getPoupanca).toEqual(0);
-   expect(component.getCarteira).toEqual(60);
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  });
+
+  it(`(U) setSacar(): poupanca don't have string (isNan) or poupanca < value`, () => {
+    expect(component.setSacar('string')).not.toBeTruthy()
+    expect(component.setSacar('100')).not.toBeTruthy()
+
+    expect(component.getPoupanca).toEqual(10);
+    expect(component.getCarteira).toEqual(50);
   });
 
   it('(U) setDepositar(): should transfer carteira from poupanca', () => {
     component.setDepositar('50');
- 
+
     expect(component.getCarteira).toEqual(0);
     expect(component.getPoupanca).toEqual(60);
-   });
+  });
+
+  it(`(U) setDepositar(): carteira don't have string (isNan) or carteira < value`, () => {
+    expect(component.setDepositar('string')).not.toBeTruthy()
+    expect(component.setDepositar('100')).not.toBeTruthy()
+
+    expect(component.getPoupanca).toEqual(10);
+    expect(component.getCarteira).toEqual(50);
+  });
 });
