1509a1706f8451021e618ca3c37894feec27e207
Paulo Almeida <estpaulovictora@live.com>
2021-09-12 13:37:25 -0300

    testes unitarios e code-coverage

---
+++ teste-unitario/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'teste-unitario';
+
+  public soma(value1: number, value2: number): number {
+    return value1+value2;
+  }
+}
